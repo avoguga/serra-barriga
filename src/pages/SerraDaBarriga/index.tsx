360f68ddd74d034a59e174a453f16474a43fe3c9
Aryadne Coppini <arya.kusse@gmail.com>
2024-01-29 18:48:24 -0300

    Feat: Fazendo a estruturação da página.

--- src/pages/SerraDaBarriga/index.tsx
+++ src/pages/SerraDaBarriga/index.tsx
@@ -1,5 +1,21 @@
+import './style.css/';
+import logo from '../../assets/app_serra.svg';
+import setaEsquerda from '../../assets/set_esquerda.svg';
+import setaDireita from '../../assets/set_direita.svg';
+
 const SerraDaBarriga = () => {
-  return <div>SerraDaBarriga</div>;
+  return (
+    <div>
+      <img src={logo} className="logoStyle"/>
+      <p><strong>Serra da Barriga</strong> está localizada no atual município de União dos Palmares, no estado de Alagoas. À época do Quilombo dos Palmares, fazia parte da Capitania de Pernambuco. Foi tombada pelo Instituro do Patrimônio Histórico e Artístico Nacional em 1986.</p>
+      <div>fotos</div>
+      <button><img src={setaEsquerda}/></button>
+      <button><img src={setaDireita}/></button>
+      <div>vídeos</div>
+      <button><img src={setaEsquerda}/></button>
+      <button><img src={setaDireita}/></button>
+    </div>
+    );
 };
 
 export default SerraDaBarriga;
