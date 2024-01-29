3477cbdc4d7a66cc6ba2f14db80aeeda3f5606b6
clauds-macedo <yanalogic@gmail.com>
2024-01-29 11:55:36 -0300

    feat: adiciona lib para leitor de qr code

--- src/pages/QRCode/index.tsx
+++ src/pages/QRCode/index.tsx
@@ -1,5 +1,24 @@
-const QRCode = () => {
-  return <div>QRCode</div>;
-};
+import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
 
+const QRCode = () => {
+  const [data, setData] = useState('No result');
+
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (result) {
+            setData(result.getText());
+            return;
+          }
+          console.info(error);
+        }}
+        constraints={{}}
+        containerStyle={{ width: 300, height: 300 }}
+      />
+      <p>{data}</p>
+    </>
+  );
+};
 export default QRCode;
