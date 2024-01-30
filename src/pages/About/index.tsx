37aacfe6c43ea2a27c312cfc90b08349610d0c3a
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2024-01-30 13:13:10 -0300

    Insere fundo e inicia estrutura de sobre

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,28 @@
+import { styled } from 'styled-components';
+import appSerra from '../../assets/icons/app serra.svg';
+import WatermarkWrapper from '../../components/WatermarkWrapper/WatermarkWrapper';
+import WatermarkImage from '../../assets/background-red.png';
+
+const Content = styled.div`
+  background-color: #8D094A;
+  height: 100vh;
+`;
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <WatermarkWrapper watermarkImage={WatermarkImage} watermark={true}>
+      <Content>
+      <img
+          src={appSerra}
+          alt="Logo do aplicativo Serra da Barriga"
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </Content>
+    </WatermarkWrapper>
+  );
 };
 
 export default About;
