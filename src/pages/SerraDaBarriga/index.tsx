4a7086610d45b2566b29c633bd049646f566764a
Aryadne Coppini <arya.kusse@gmail.com>
2024-01-30 17:57:25 -0300

    Feat: Finalização da estrutura geral da página.

--- src/pages/SerraDaBarriga/index.tsx
+++ src/pages/SerraDaBarriga/index.tsx
@@ -2,18 +2,48 @@ import './style.css/';
 import logo from '../../assets/app_serra.svg';
 import setaEsquerda from '../../assets/set_esquerda.svg';
 import setaDireita from '../../assets/set_direita.svg';
+import video from '../../assets/teste_video.mp4'
+import teste1 from '../../assets/teste1.jpg';
+import teste2 from '../../assets/teste2.jpg';
+import teste3 from '../../assets/teste3.jpg';
+import teste4 from '../../assets/teste4.jpg';
+import teste5 from '../../assets/teste5.jpg';
+import teste6 from '../../assets/teste6.jpg';
+
+
 
 const SerraDaBarriga = () => {
+  const images = [
+    {src: teste1},
+    {src: teste2},
+    {src: teste3},
+    {src: teste4},
+    {src: teste5},
+    {src: teste6}
+  ];
+
   return (
-    <div>
+    <div className='container'>
       <img src={logo} className="logoStyle"/>
-      <p><strong>Serra da Barriga</strong> está localizada no atual município de União dos Palmares, no estado de Alagoas. À época do Quilombo dos Palmares, fazia parte da Capitania de Pernambuco. Foi tombada pelo Instituro do Patrimônio Histórico e Artístico Nacional em 1986.</p>
-      <div>fotos</div>
+      <div className='text'>
+        <strong>Serra da Barriga</strong> está localizada no atual município de União dos Palmares, no estado de Alagoas. À época do Quilombo dos Palmares, fazia parte da Capitania de Pernambuco. Foi tombada pelo Instituto do Patrimônio Histórico e Artístico Nacional em 1986.
+      </div>
+      <div className='title'>FOTOS</div>
+      <div className='carrosel'>
+        {images.map((image, index) => (
+          <img key={index} src={image.src} className='image'/>
+        ))}
+      </div>
+      <div className="btnSeta">
       <button><img src={setaEsquerda}/></button>
       <button><img src={setaDireita}/></button>
-      <div>vídeos</div>
+      </div>
+      <div className='title'>VÍDEOS</div>
+      <video src={video} height="170px" controls className='video'></video>
+      <div className="btnSeta">
       <button><img src={setaEsquerda}/></button>
       <button><img src={setaDireita}/></button>
+      </div>
     </div>
     );
 };
