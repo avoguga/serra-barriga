5e8da8edc8a0cd2afa44618db44704e26963fd10
Aryadne Coppini <t-aparecida.coppini@cni.com.br>
2024-02-05 10:35:39 -0300

    Criado componente do container transparente.

--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -1,29 +1,19 @@
 import WatermarkWrapper from '../../components/WatermarkWrapper/WatermarkWrapper';
-import styled from 'styled-components';
 import tiktok from '../../assets/icons/app serra.svg';
 import { SelfieContainer } from '../HistorySelfie';
 import { Icons } from '../../helpers/icons';
+import './style.css';
 
 const VideoIcon = Icons['View360'];
 
 const Videos = () => {
   return (
     <WatermarkWrapper>
-      <div
-        style={{
-          backgroundColor: '#8AA61E',
-          height: '100vh',
-        }}
-      >
+      <div className="background">
         <img
           src={tiktok}
           alt="tiktok"
-          style={{
-            width: '100%',
-            height: '102px',
-            marginTop: '118px',
-            marginBottom: '67px',
-          }}
+          className='appImg'
         />
         <SelfieContainer>
           <VideoIcon />
