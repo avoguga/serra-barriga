aadb816589d17c3ddb3513a90768ac5a40a6933a
Aryadne Coppini <arya.kusse@gmail.com>
2024-02-07 16:30:09 -0300

    Finalização da tela principal.

--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -1,18 +1,30 @@
 import WatermarkWrapper from '../../components/WatermarkWrapper/WatermarkWrapper';
 import tiktok from '../../assets/icons/app serra.svg';
 import { SelfieContainer } from '../HistorySelfie';
+import seta from '../../assets/seta voltar e abaixo - branco.svg';
+import { useNavigate } from 'react-router-dom';
 import Video from '../../assets/icons/video.svg';
 import './style.css';
 
 const Videos = () => {
+  const navigate = useNavigate();
   return (
     <WatermarkWrapper>
+
       <div className="background">
+      <button className='back' 
+        onClick={() => {
+          navigate('/takehome');
+        }}>
+            <img src={seta} alt="" />
+        </button>
+
         <img
           src={tiktok}
           alt="tiktok"
           className='appImg'
         />
+        
         <SelfieContainer
           style={{
             backgroundColor: '#B75C01'
@@ -21,14 +33,16 @@ const Videos = () => {
           <img src={Video} className='video-icon'/>
           <p className='text-video'>VÍDEOS</p>
         </SelfieContainer>
+
         <div className='video-containers'>
           <span className='video1'/>
-          <p className='text-video'>Vídeo 1</p>
+          <p className='text-video-obj'>Vídeo 1</p>
           <span className='video2'/>
-          <p className='text-video'>Vídeo 2</p>
+          <p className='text-video-obj'>Vídeo 2</p>
           <span className='video3'/>
-          <p className='text-video'>Vídeo 3</p>
+          <p className='text-video-obj'>Vídeo 3</p>
         </div>
+
       </div>
     </WatermarkWrapper>
   );
