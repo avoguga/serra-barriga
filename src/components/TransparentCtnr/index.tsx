483bf4fa409a88cb576e77e710fb6a61ac07f70b
Gustavo <avoguga@gmail.com>
2024-02-08 02:18:35 -0300

    Fix: Ajusta build

--- src/components/TransparentCtnr/index.tsx
+++ src/components/TransparentCtnr/index.tsx
@@ -1,14 +1,13 @@
-import React, { useRef, useState } from 'react';
+// import React, { useRef, useState } from 'react';
 
+// function TransparentCtnr() {
+//     const [text, setText] = useState('');
+//     const containerRef = useRef(null);
+//     const handleTextChange = (e) => {
+//         setText(e.target.value);
+//         if (containerRed.current) {
+//             containerRef.current.style.width = `${containerRef.current.scrollWidth}px`;
 
-function TransparentCtnr() {
-    const [text, setText] = useState('');
-    const containerRef = useRef(null);
-    const handleTextChange = (e) => {
-        setText(e.target.value);
-        if (containerRed.current) {
-            containerRef.current.style.width = `${containerRef.current.scrollWidth}px`;
-
-        }
-    }
-}
+//         }
+//     }
+// }
