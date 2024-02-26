15378b2ccb49acc1dd218e2a83539991abe2bc11
Aryadne Coppini <arya.kusse@gmail.com>
2024-02-26 18:12:21 -0300

    Criado componente para rolar a página pra baixo.

---
+++ src/components/ScrollButton/index.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import DownArrow from '../../assets/icons/seta baixo.svg';
+  
+  const ArrowButton = styled.button`
+    position: fixed;
+    width: 100%;
+    left: 50%;
+    bottom: 40px;
+    height: 20px;
+    font-size: 3rem;
+    z-index: 1;
+    cursor: pointer;
+    color: #394404;
+  `;
+  
+  const BtnDownArrow = () => {
+    const [visible, setVisible] = useState(false);
+  
+    const toggleVisible = () => {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled <= 300) {
+        setVisible(true);
+      } else if (scrolled > 300) {
+        setVisible(false);
+      }
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', toggleVisible);
+      return () => window.removeEventListener('scroll', toggleVisible);
+    }, []);
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth',
+      });
+    };
+  
+    return (
+      <ArrowButton 
+        onClick={scrollToTop} 
+        style={{ display: visible ? 'inline' : 'none' }}>
+        <img 
+            src={DownArrow} 
+            alt={'Seta para descer a página.'} 
+            />
+      </ArrowButton>
+    );
+  };
+  
+  export default BtnDownArrow;
