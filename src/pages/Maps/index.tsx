b1727bb5d7c7f57ae77cbfac3cd6706f4b3cc625
João Mendes <111240871+joaosmendess@users.noreply.github.com>
2024-03-01 14:18:54 -0300

    Update index.tsx

--- src/pages/Maps/index.tsx
+++ src/pages/Maps/index.tsx
@@ -1,183 +1,165 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import WatermarkWrapper from '../../components/WatermarkWrapper/WatermarkWrapper';
-import styled from 'styled-components';
-import tiktok from '../../assets/logo.png';
-import mapa from '../../assets/mapa.png';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as tmImage from '@teachablemachine/image';
+import logo from '../../assets/logo.png';
 import WatermarkImage from '../../assets/marcadaguaverdeescuro.png';
+import { CameraContainer, Container, HeaderContainer } from './styles';
+import { BackButton } from '../TakeHome';
 import seta from '../../assets/seta voltar e abaixo - branco.svg';
-//import { Icons } from '../../helpers/icons';
-import LocIcon from '../../assets/icons/localização.svg';
-import Pointer from '../../assets/icons/dedo clicando - branco.svg';
-import BtnDownArrow from '../../components/ScrollButton';
+import WatermarkWrapper from '../../components/WatermarkWrapper/WatermarkWrapper';
+import { getEspacoData } from '../../helpers/Espacos';
 
-//const HomeIcon = Icons['Arqueologia'];
+// Interface para as previsões
+interface Prediction {
+  className: string;
+  probability: number;
+}
 
-const ButtonContainer = styled.div`
-  display: flex;
-  background-color: #67781b;
-  flex-wrap: wrap;
-  gap: 20px;
-  justify-content: center;
-  margin: 0 10px;
-`;
-
-export const BackButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--square-color);
-  border-radius: 50%;
-  width: 55px;
-  height: 55px;
-  position: fixed; // Alterado de absolute para fixed
-  top: 7%;
-  left: 5%;
-  z-index: 10;
-  cursor: pointer;
-`;
-
-const Item = styled.p<{ bgColor: string }>`
-  background-color: ${(props) => props.bgColor};
-  color: white;
-  width: 100%;
-  text-align: center;
-  padding: 10px 0;
-  margin: 0;
-`;
-
-const items = [
-  'ENTRADA',
-  'OXILE DAS ERVAS',
-  'ONJÓ CRUZAMBÊ',
-  'ESPAÇO ACOTIRENE',
-  'MUXIMA DE PALMARES',
-  'ESPAÇO QUILOMBO',
-  'ATALAIA DE ACAIENE',
-  'ESPAÇO GANGA-ZUMBA',
-  'LAGOA ENCANTADA DOS NEGROS',
-  'ESPAÇO AQUALTUNE',
-  'OCAS INDÍGENAS',
-  'ESPAÇO CAÁ-PUÊRA',
-  'BATUCAJÉ',
-  'ATALAIA DO ACAIUBA',
-  'ONJÓ DE FARINHA',
-  'ESPAÇO ZUMBI',
-  'ATALAIA DO TOCULO',
-  'RESTAURANTE KÚUKU-WAANA',
-];
-
-const Maps = () => {
+const QRCode = () => {
   const navigate = useNavigate();
+  const webcamRef = useRef<HTMLDivElement>(null);
+  const URL = 'https://teachablemachine.withgoogle.com/models/07fghDy7n/';
+  const [model, setModel] = useState<tmImage.CustomMobileNet | null>(null);
+  const [webcam, setWebcam] = useState<tmImage.Webcam | null>(null); // Estado para a webcam
+  const [isLoading, setIsLoading] = useState(true); // Novo estado para controle de carregamento
+  const [cameraActive, setCameraActive] = useState(true); // Estado para controlar a atividade da câmera
 
-  const getBackgroundColor = (index: number) => {
+  useEffect(() => {
+    const constraints = { video: true };
+  
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        // Encontrar o elemento de vídeo no DOM
+        const videoElement = webcamRef.current?.querySelector('video');
+        if (videoElement) {
+          videoElement.srcObject = stream;
+          videoElement.play();
+        }
+      })
+      .catch((error) => {
+        console.error('Erro ao acessar a câmera:', error);
+      });
+  }, []);
 
- 
-  const getBackgroundColor = (index: number) => {
-    const colors = ['#586617', '#67781B'];
-    return colors[index % colors.length];
-  };
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        
+        const video = document.querySelector('video');
+        if (video) {
+          video.srcObject = stream;
+        }
+      })
+      .catch(err => {
+        console.error("Erro ao acessar a câmera: ", err);
+      });
+  }, []);
 
+  useEffect(() => {
+    async function loadModel() {
+      const modelURL = URL + 'model.json';
+      const metadataURL = URL + 'metadata.json';
+      const loadedModel = await tmImage.load(modelURL, metadataURL);
+      setModel(loadedModel);
+    }
+
+    loadModel();
+  }, [URL]);
+
+  const handlePrediction = useCallback(
+    (predictions: Prediction[]) => {
+      predictions.forEach(({ className, probability }) => {
+        if (probability > 0.8) {
+          console.log(`Classe prevista: ${className}, Probabilidade: ${probability}`);
+
+          const espacoData = getEspacoData(className);
+
+          if (espacoData) {
+            console.log(`Navegando para ${espacoData.title}: ${espacoData.description}`);
+            navigate(espacoData.path);
+            setCameraActive(false); // Desativa a câmera se a condição for atendida
+            setIsLoading(false); // Desativa o loading
+          }
+        }
+      });
+    },
+    [navigate]
+  );
+
+  useEffect(() => {
+    if (!cameraActive) return;
+
+    let isComponentMounted = true;
+    setIsLoading(true); // Iniciar o carregamento
+
+    async function setupWebcam() {
+      if (!model || !cameraActive) return;
+      const newWebcam = new tmImage.Webcam();
+      try {
+        await newWebcam.setup({ facingMode: 'environment' }); // Preferência pela câmera frontal
+        await newWebcam.play();
+        setWebcam(newWebcam);
+
+        if (webcamRef.current) {
+          webcamRef.current.appendChild(newWebcam.canvas);
+          newWebcam.canvas.style.position = 'absolute';
+          newWebcam.canvas.style.width = '100%';
+          newWebcam.canvas.style.height = '100%';
+          newWebcam.canvas.style.objectFit = 'cover';
+          newWebcam.canvas.style.top = '0';
+          newWebcam.canvas.style.left = '0';
+          const videoElement = document.createElement('video');
+          videoElement.style.width = '100%';
+          videoElement.style.height = '100%';
+          videoElement.playsInline = true;
+          videoElement.autoplay = true;
+          videoElement.muted = true; // Necessário para autoplay em alguns navegadores
+       
+          webcamRef.current.appendChild(videoElement);
+        }
+
+        setIsLoading(false); // Finaliza o carregamento quando a câmera está pronta
+
+        const loop = async () => {
+          if (!isComponentMounted || !model || !cameraActive) return;
+
+          newWebcam.update();
+          const prediction = await model.predict(newWebcam.canvas) as Prediction[];
+          handlePrediction(prediction);
+          if (cameraActive) requestAnimationFrame(loop);
+        };
+
+        loop();
+      } catch (error) {
+        console.error('Erro ao configurar a webcam:', error);
+        setIsLoading(false); // Finaliza o carregamento se houver um erro
+      }
+    }
+
+    if (model && cameraActive) {
+      setupWebcam();
+    }
+
+    return () => {
+      isComponentMounted = false;
+      webcam?.stop();
+    };
+  }, [model, cameraActive, handlePrediction]);
 
   return (
-    <>
-      <Outlet />
-      <WatermarkWrapper watermarkImage={WatermarkImage}>
-        <div
-          style={{
-            backgroundColor: '#67781B',
-            height: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <BackButton
-            onClick={() => {
-              navigate('/takehome/serra-da-barriga');
-            }}
-          >
-            <img src={seta} alt="" />
+    <WatermarkWrapper watermarkImage={WatermarkImage} watermark>
+      <Container>
+        <HeaderContainer>
+          <BackButton onClick={() => navigate('/')}>
+            <img src={seta} alt="Seta voltar" />
           </BackButton>
-          <img
-            onClick={() => {
-              navigate('/');
-            }}
-            src={tiktok}
-            alt="tiktok"
-            style={{
-              width: '200px',
-              height: '70px',
-              marginTop: '50px',
-              marginBottom: '60px',
-            }}
-          />
-
-          <ButtonContainer>
-            <img
-              src={LocIcon}
-              style={{
-                width: '30px',
-                height: '30px',
-                marginRight: '-10px',
-                marginTop: '2px',
-              }}
-            />
-            <h2
-              style={{
-                font: 'normal normal 500 22px/30px ',
-                letterSpacing: '0px',
-                color: '#FFFFFF',
-                fontSize: '30px',
-              }}
-            >
-              Mapa
-            </h2>
-
-            <img
-              onClick={() => {
-                navigate('/');
-              }}
-              src={mapa}
-              alt="mapa"
-              style={{
-                width: '500px',
-                height: '520px',
-                marginTop: '-60px',
-                marginBottom: '50px',
-              }}
-            />
-            <img
-              src={Pointer}
-              style={{
-                width: '65px',
-                height: '65px',
-                marginTop: '-140px',
-                marginBottom: '-10px',
-              }}
-            />
-            <BtnDownArrow />
-          </ButtonContainer>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '100%',
-              backgroundColor: '#67781B',
-              marginTop: '-30px',
-            }}
-          >
-            {items.map((item, index) => (
-              <Item key={index} bgColor={getBackgroundColor(index)}>
-                {index + 1} - {item}
-              </Item>
-            ))}
-          </div>
-        </div>
-      </WatermarkWrapper>
-    </>
+          <img src={logo} alt="Logo" style={{ width: '200px', height: '70px', marginTop: '50px', marginBottom: '30px' }} />
+        </HeaderContainer>
+        {isLoading && <div style={{ textAlign: 'center', fontFamily: 'FuturaPT' , fontSize: '18px' }}>Carregando a câmera, aguarde um momento...</div>}
+        <CameraContainer ref={webcamRef}></CameraContainer>
+      </Container>
+    </WatermarkWrapper>
   );
 };
 
-export default Maps;
+export default QRCode;
