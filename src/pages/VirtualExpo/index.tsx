07a403881f5629edcf74a954982286d00b001da8
joaosmendess <joaosilva0721@gmail.com>
2024-03-27 22:33:17 -0300

    feat/adiciona botao para baixar modelo 3d

--- src/pages/VirtualExpo/index.tsx
+++ src/pages/VirtualExpo/index.tsx
@@ -21,40 +21,16 @@ import obj13 from '../../assets/Objeto_15_4k-b.glb';
 import { BackButton } from '../TakeHome';
 import { useNavigate } from 'react-router-dom';
 import seta from '../../assets/seta voltar e abaixo - branco.svg';
-import {  useState } from 'react';
+import { useState } from 'react';
 
 import * as C from './styles';
-
 
 type Modelo3D = {
   id: number;
   src: string;
   alt: string;
-  isLoading: boolean;
-  progress: number;
-  isLoaded: boolean;
+  isVisible: boolean;
 };
-
-const LoadingBar = ({ progress }: any) => (
-  <div
-    style={{
-      width: '100%',
-      backgroundColor: '#ccc',
-      borderRadius: '5px',
-    
-      
-    }}
-  >
-    <div
-      style={{
-        height: '10px',
-        width: `${progress}%`,
-        backgroundColor: '#90caf9',
-        transition: 'width 0.5s ease-in-out',
-      }}
-    />
-  </div>
-);
 
 const VirtualExpo = () => {
   const ExpoIcon = Icons['Expo'];
@@ -65,139 +41,95 @@ const VirtualExpo = () => {
   const [modelos, setModelos] = useState<Modelo3D[]>([
     {
       id: 1,
-      isLoading: false,
-      progress: 0,
+isVisible:false,
       src: obj1,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 2,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj2,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 3,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj3,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 4,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj4,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 5,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj5,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 6,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj6,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 7,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj7,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 8,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj8,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 9,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj9,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 10,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj10,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 11,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj11,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 12,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj12,
       alt: 'Modelo',
-      isLoaded: false,
     },
     {
       id: 13,
-      isLoading: false,
-      progress: 0,
+      isVisible:false,
+
       src: obj13,
       alt: 'Modelo',
-      isLoaded: false,
     },
   ]);
-
-  const handleLoadModel = (modelId: number) => {
-    setModelos((currentModelos) =>
-      currentModelos.map((model) =>
-        model.id === modelId ? { ...model, isLoading: true } : model
-      )
-    );
-
-    // Simulação do carregamento
-    let fakeProgress = 0;
-    const interval = setInterval(() => {
-      fakeProgress += 10;
-      if (fakeProgress >= 100) {
-        clearInterval(interval);
-        setModelos((currentModelos) =>
-          currentModelos.map((model) =>
-            model.id === modelId
-              ? { ...model, isLoading: false, progress: 100, isLoaded: true }
-              : model
-          )
-        );
-      } else {
-        setModelos((currentModelos) =>
-          currentModelos.map((model) =>
-            model.id === modelId ? { ...model, progress: fakeProgress } : model
-          )
-        );
-      }
-    }, 100);
-  };
 
   const fullText = `A Serra da Barriga possui um patrimônio arqueológico que conta a história de diversos povos que aqui habitavam em tempos passados. A sua passagem por essas terras foi preservada através de diversos vestígios que resistiram ao tempo. \n\n
    Estudos arqueológicos realizados pelo Núcleo de Ensino e Pesquisa Arqueológico da Universidade Federal de Alagoas, revelaram cachimbos, panelas de barro, ferramentas de pedra lascada e polida, marcas dos pisos de moradias e muito mais. Essas evidências permitem que se aprenda muito sobre a vida cotidiana desses grupos.`;
@@ -213,6 +145,17 @@ const VirtualExpo = () => {
     : paragraphs.slice(0, previewParagraphCount);
 
   const Download = Icons['DownloadWhite'];
+
+  const handleLoadModel =  (id:number) => {
+    setModelos(modelos.map(modelo => {
+      if(modelo.id === id) {
+        return{...modelo, isVisible: !modelo.isVisible};
+      }
+      return modelo;
+    }))
+
+  }
+  
 
   return (
     <WatermarkWrapper>
@@ -273,43 +216,56 @@ const VirtualExpo = () => {
         <C.ModelsContainer>
           {modelos.map((modelo) => (
             <div key={modelo.id}>
-              {!modelo.isLoaded &&
-                !modelo.isLoading &&
-                modelo.progress < 100 && (
+              <model-viewer
+                ar
+             
+                camera-controls
+                touch-action="pan-y"
+                src={modelo.src}
+                alt={modelo.alt}
+                style={{
+                  width: '146px',
+                  height: '146px',
+                  backgroundColor: '#EB7400',
+
+                  borderRadius: '50%',
+                }}
+              >
+                <div>
                   <button
-                    onClick={() => handleLoadModel(modelo.id)}
-                    
-                  >
-                    <Download title="Carregar modelo" />
-                  </button>
-                )}
-
-              {modelo.isLoading && modelo.progress < 100 && (
-                <LoadingBar progress={modelo.progress} />
-              )}
-
-              {modelo.progress === 100 && (
-                <div
-                  style={{
-                    background: ' #EB7400',
-                    width: '100%',
-                    height: '100%',
-                    borderRadius: '300px',
-                    display: 'block',
-                  }}
-                >
-                  <model-viewer
-                    ar
-                    camera-controls
-                    src={modelo.src}
-                    alt={modelo.alt}
                     style={{
+                  backgroundColor: '#753D00',
+                  
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
                       width: '100%',
-                      height: '100%',
+                      height: '150px',
+                    
+                      borderRadius: '5px',
+                      opacity: '0.97',
                     }}
-                  />
+                    onClick={ ()=> handleLoadModel(modelo.id)}
+                  >
+                    {modelo.isVisible ?
+                     <model-viewer
+                     ar
+                     lazy-load
+                     camera-controls
+                     src={modelo.src}
+                     alt={modelo.alt}
+                     style={{
+                       width: '146px',
+                       height: '146px',
+                       backgroundColor: '#EB7400',
+     
+                       borderRadius: '50%',
+                     }}
+                   /> : 'Mostrar Modelo'}
+                
+                  </button>
                 </div>
-              )}
+              </model-viewer>
             </div>
           ))}
         </C.ModelsContainer>
