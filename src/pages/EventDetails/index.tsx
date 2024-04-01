b744574eff98a98ba728f72807b10852bd81aaf0
joaosmendess <joaosilva0721@gmail.com>
2024-04-01 20:51:25 -0300

    feat/Implementa gereciador de agenda no front end

--- src/pages/EventDetails/index.tsx
+++ src/pages/EventDetails/index.tsx
@@ -1,38 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { agenda } from '../../helpers/agenda';
+import axios from 'axios';
 
 import * as C from './styles';
 import Header from '../../components/Header';
+import {format} from 'date-fns'
+
+interface EventoProps {
+  id: number;
+  Title: string;
+  Month: string;
+  DescriptionPT: string;
+  DescriptionEN: string;
+
+  Data: Date;
+}
 
 const EventDetails = () => {
-  const { eventId } = useParams();
+  const { eventId } = useParams<{ eventId?: string }>(); // Garanta que o useParams reflita o tipo correto
+  const [evento, setEvento] = useState<EventoProps | null>(null); // O estado deve ser inicializado como null
+  const [error, setError] = useState<string>('');
 
-  const evento = agenda
-    .flatMap((mesAgenda) => mesAgenda.eventos)
-    .find((e) => e.id.toString() === eventId);
+  useEffect(() => {
+    // Certifique-se de que o ID do evento esteja presente antes de fazer a chamada da API
+    if (eventId) {
+      axios.get(`http://localhost:1337/api/agendas/${eventId}`)
+        .then(response => {
+          const eventoData: EventoProps = {
+            id: response.data.data.id,
+            Title: response.data.data.attributes.Title,
+            Month: response.data.data.attributes.Month,
+            DescriptionPT: response.data.data.attributes.DescriptionPT,
+            DescriptionEN: response.data.data.attributes.DescriptionEN,
+
+            Data: new Date(response.data.data.attributes.Data),
+          };
+          setEvento(eventoData);
+        })
+        .catch(err => {
+          setError('Evento não encontrado');
+          console.error('Erro ao buscar detalhes do evento:', err);
+        });
+    }
+  }, [eventId]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   if (!evento) {
-    return <div>Evento não encontrado</div>;
+    return <div style={{ backgroundColor: '#009289', height: '100vh'}}>Carregando...</div>;
   }
 
   return (
     <C.Container>
       <Header />
       <C.DetalhesContainer>
-        <C.TituloEvento>{evento.titulo}</C.TituloEvento>
-
+        <C.TituloEvento>{evento.Title}</C.TituloEvento>
         <C.DataHoraEvento>
-          {' '}
           <h2>
-            {evento.data} às {evento.hora}
-          </h2>{' '}
-        </C.DataHoraEvento>
-        <C.DescricaoEvento>{evento.descricao.pt}</C.DescricaoEvento>
-        <C.TituloSecaoEvento>ACAIUBA´S LOOKOUT </C.TituloSecaoEvento>
+          {format(evento.Data, 'dd/MM \'às\' HH\'h\'')}
 
+          </h2>
+        </C.DataHoraEvento>
+        <C.DescricaoEvento>{evento.DescriptionPT}</C.DescricaoEvento>
+        <C.TituloSecaoEvento></C.TituloSecaoEvento>
         <C.DescricaoEvento>
-          <span>{evento.descricao.en}</span>
+          <span>{evento.DescriptionEN}</span>
         </C.DescricaoEvento>
       </C.DetalhesContainer>
     </C.Container>
